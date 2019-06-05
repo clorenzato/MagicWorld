dd764d38f9f906dc9c1d586a4944ee48d208d13e
clorenzato <cyril.lorenzato@gmail.com>
2019-06-06 00:24:27 +0200

    Add new class Area the place where J1 & J2 will fight Also Working on the constructor of the Arena.

--- src/com/lorenzato/Main.java
+++ src/com/lorenzato/Main.java
@@ -1,8 +1,11 @@
 package com.lorenzato;
 
+import com.lorenzato.arena.Arena;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Tala!");
+        System.out.println("Bienvenue dans MagiWorld !!");
+        Arena arene = new Arena;
     }
 }
---
+++ src/com/lorenzato/arena/Arena.java
@@ -0,0 +1,23 @@
+package com.lorenzato.arena;
+
+import java.util.Scanner;
+
+public class Arena {
+    public Arena() {
+        System.out.println("Joueur 1 type");
+        this.addPlayerInArena();
+
+    }
+
+    private void addPlayerInArena() {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        switch (sc){
+            case 1 :
+
+                break;
+            case 2 :
+                break;
+        }
+    }
+}

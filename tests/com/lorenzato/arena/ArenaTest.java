a73285041436db059416e9f819b6bc1e5247e617
clorenzato <cyril.lorenzato@gmail.com>
2019-06-09 11:07:23 +0200

    Adding some test of the ArenaClass

--- tests/com/lorenzato/arena/ArenaTest.java
+++ tests/com/lorenzato/arena/ArenaTest.java
@@ -12,24 +12,44 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ArenaTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
     @BeforeEach
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
     }
+
 
     @AfterEach
     public void restoreStreams() {
         System.setOut(System.out);
+        System.setErr(System.err);
+    }
+
+
+    @Test
+    void Given_GoodEntries_When_SelectPersoClass_Then_ProposeClassinGoodOrder() {
+        System.setIn(new ByteArrayInputStream(String.format("2%n100%n20%n20%n20%n").getBytes()));
+        Arena arena = new Arena(1,100);
+        arena.choosePersoClass(1);
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals("1 - Guerrier", output[11]);
+        assertEquals("2 - Rôdeur", output[12]);
+        assertEquals("3 - Mage", output[13]);
     }
 
     @Test
-    void addPlayerInArena() {
-        System.setIn(new ByteArrayInputStream(String.format("2%n100%n20%n20%n20%n").getBytes()));
+    void Given_BadEntries_When_SelectPersoClass_Then_CatchErrs() {
+        System.setIn(new ByteArrayInputStream(String.format("a%n25%n2%n100%n20%n20%n20%n").getBytes()));
         Arena arena = new Arena(1,100);
-        arena.addPlayersInArena();
-        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
-        // TODO: 07/06/19 Finish the Test!! 
-        assertEquals("Vous devez saisir un nombre, correspondant au nombre de menus souhaités", output[15]);
+        arena.choosePersoClass(1);
+        String[] errput = errContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals("Veulliez saisir un chiffre, svp", errput[0]);
+        assertEquals("Vous n'avez pas choisi de class parmi les choix proposés", errput[1]);
+    }
+
+    @Test
+    void chooseAttribute() {
     }
 }

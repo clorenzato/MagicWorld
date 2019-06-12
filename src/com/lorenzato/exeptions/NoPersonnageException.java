8c013440735fe38d4787f2912caa173ae5ae0557
clorenzato <cyril.lorenzato@gmail.com>
2019-06-12 16:15:14 +0200

    Typo Corrections

--- src/com/lorenzato/exeptions/NoPersonnageException.java
+++ src/com/lorenzato/exeptions/NoPersonnageException.java
@@ -2,6 +2,6 @@ package com.lorenzato.exeptions;
 
 public class NoPersonnageException extends RuntimeException {
     public NoPersonnageException() {
-        System.err.println("Erreur Fatale : La Liste des joueurs est vide. Veuillez appeler la methode addPlayersInArena avant de lancer le combat.");
+        System.err.println(" Fatal Error : La Liste des joueurs est vide. Veuillez appeler la méthode addPlayersInArena avant de lancer le combat.");
     }
 }

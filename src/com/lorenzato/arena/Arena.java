9d2656cbfbc8ec84fa6b8c67071b534739adb301
clorenzato <cyril.lorenzato@gmail.com>
2019-06-13 13:46:54 +0200

    - Factorisation of the addPlayerInArena method => new method addPersonageToList - Adding increaseAttribute method to force the player to use          all of its attributes points.
    + Typo corrections

--- src/com/lorenzato/arena/Arena.java
+++ src/com/lorenzato/arena/Arena.java
@@ -35,7 +35,7 @@ public class Arena {
 
     /**
      * Ask the user to give a number in the console. Check if the result is a number.
-     * @return the number that have been chosen
+     * @return the number that have been chosen.
      */
     int chooseNumber(){
 
@@ -56,36 +56,58 @@ public class Arena {
     }
 
     /**
-     * Add as many Personage in the arena as the number of player. Call all function needed to construct a Player and
+     * Add as many Personage in the arena as the number of player (nbPlayer).
+     * Call all function needed to construct a Player and
      * add it to the list.
      */
     public void addPlayersInArena() {
 
         for (int i = 1; i < this.nbPlayer+1; i++) {
 
-            int playerClass = choosePersonageClass(i);
+            int personageClass = choosePersonageClass(i);
             int choiceLevel = choosePersonageLevel();
             int strength = chooseAttribute("Force");
             int agility = chooseAttribute("Agilité");
             int intelligence = chooseAttribute("Intelligence");
 
-            switch (playerClass){
-                case 1 :
-                    Warrior warrior = new Warrior(choiceLevel,strength,agility,intelligence);
-                    warrior.personageDescription(i);
-                    personages.add(warrior);
-                    break;
-                case 2 :
-                    Prowler prowler = new Prowler(choiceLevel,strength,agility,intelligence);
-                    prowler.personageDescription(i);
-                    personages.add(prowler);
-                    break;
-                case 3 :
-                    Mage mage = new Mage(choiceLevel,strength,agility,intelligence);
-                    mage.personageDescription(i);
-                    personages.add(mage);
-                    break;
+
+            while (strength + agility + intelligence != choiceLevel) {
+                System.out.println("Il vous reste "+ this.attributesAvailable + " pts à attribuer");
+                strength = increaseAttribute("Force",strength);
+                agility = increaseAttribute("Agilité",agility);
+                intelligence = increaseAttribute("Intelligence", intelligence);
             }
+
+            addPersonnageToList(i, personageClass, choiceLevel, strength, agility, intelligence);
+        }
+    }
+
+    /**
+     * Create and add a Personage to the list of personages.
+     * @param indexPlayer he number of the player e.g. "Joueur indexPlayer" == "Joueur 1" if indexPlayer = 1.
+     * @param personageClass The personage Class e.g. "Mage" if personageClass == 3.
+     * @param choiceLevel The level of the personage
+     * @param strength The strength of the personage
+     * @param agility The agility of the personage
+     * @param intelligence The intelligence of the personage
+     */
+    private void addPersonnageToList(int indexPlayer, int personageClass, int choiceLevel, int strength, int agility, int intelligence){
+        switch (personageClass){
+            case 1 :
+                Warrior warrior = new Warrior(choiceLevel,strength,agility,intelligence);
+                warrior.personageDescription(indexPlayer);
+                personages.add(warrior);
+                break;
+            case 2 :
+                Prowler prowler = new Prowler(choiceLevel,strength,agility,intelligence);
+                prowler.personageDescription(indexPlayer);
+                personages.add(prowler);
+                break;
+            case 3 :
+                Mage mage = new Mage(choiceLevel,strength,agility,intelligence);
+                mage.personageDescription(indexPlayer);
+                personages.add(mage);
+                break;
         }
     }
 
@@ -96,7 +118,7 @@ public class Arena {
      */
     int choosePersonageClass(int indexPlayer) {
 
-        int playerClass;
+        int personageClass;
         boolean choiceGood;
 
         do {
@@ -106,8 +128,8 @@ public class Arena {
             System.out.println("2 - Rôdeur");
             System.out.println("3 - Mage");
 
-            playerClass = chooseNumber();
-            choiceGood = ((playerClass >= 1) && (playerClass <= 3));
+            personageClass = chooseNumber();
+            choiceGood = ((personageClass >= 1) && (personageClass <= 3));
 
             if (!choiceGood) {
                 System.err.println("Vous n'avez pas choisi de class parmi les choix proposés");
@@ -115,7 +137,7 @@ public class Arena {
         } while(!choiceGood);
 
 
-    return playerClass;
+    return personageClass;
     }
 
     /**
@@ -149,7 +171,7 @@ public class Arena {
      * @param attributeName The Name of the attribute to display to the user
      * @return the value of the attribute (0-100).
      */
-    int chooseAttribute(String attributeName) {
+    private int chooseAttribute(String attributeName) {
 
         if (attributesAvailable != 0) {
 
@@ -160,9 +182,9 @@ public class Arena {
                 boolean isVowel = "aeiouyAEIOUY".contains(Character.toString(attributeName.charAt(0)));
 
                 if (isVowel)
-                    System.out.println("Veuillez choisir l'"+ attributeName +" du personnage (0-"+ attributesAvailable +"; MAX = 100):");
+                    System.out.println("Veuillez choisir l'"+ attributeName +" du personnage (0-"+ attributesAvailable + "; MAX = 100):");
                 else
-                    System.out.println("Veuillez choisir la "+ attributeName +" du personnage (0-"+ attributesAvailable +"; MAX = 100):");
+                    System.out.println("Veuillez choisir la "+ attributeName +" du personnage (0-"+ attributesAvailable + "; MAX = 100):");
 
                 attribute = chooseNumber();
                 attributeGood = ((attribute >= 0) && (attribute <= attributesAvailable));
@@ -193,6 +215,58 @@ public class Arena {
         } else {
             System.out.println("Plus de point d'attribut; " + attributeName + " = 0");
             return 0;
+        }
+    }
+
+    /**
+     * Allow for select the value of a given (attributeName) attribute
+     * @param attributeName The Name of the attribute to display to the user
+     * @return the value of the attribute (0-100).
+     */
+    private int increaseAttribute(String attributeName, int currentAttributeValue) {
+
+        if (attributesAvailable != 0) {
+
+            int attribute;
+            boolean attributeGood;
+
+            do {
+                boolean isVowel = "aeiouyAEIOUY".contains(Character.toString(attributeName.charAt(0)));
+
+                if (isVowel)
+                    System.out.println("Veuillez choisir l'"+ attributeName +" à ajouter (0-"+ attributesAvailable +";  "+ attributeName + " actuelle = " + currentAttributeValue + " pts; MAX = 100):");
+                else
+                    System.out.println("Veuillez choisir la "+ attributeName +" à ajouter (0-"+ attributesAvailable +";  "+ attributeName + " actuelle = " + currentAttributeValue + " pts; MAX = 100):");
+
+                attribute = chooseNumber();
+                attributeGood = ((attribute >= 0) && (attribute <= attributesAvailable));
+
+                if ((attribute + currentAttributeValue) < 0) {
+                    if (isVowel)
+                        System.err.println("L'"+ attributeName +" totale doit être >= 0");
+                    else
+                        System.err.println("La "+ attributeName +" totale doit être >= 0");
+
+                } else if ((attribute + currentAttributeValue) > 100) {
+                    if (isVowel)
+                        System.err.println("L'"+ attributeName +" totale doit être <= 100");
+                    else
+                        System.err.println("La "+ attributeName +" total doit être <= 100");
+                } else if ((attribute > attributesAvailable))
+                    System.err.println("Pas assez de point d'atribut restant! ");
+            } while(!attributeGood);
+
+            if ((attribute + currentAttributeValue) <= 100) {
+                attributesAvailable -= attribute;
+                return (attribute + currentAttributeValue);
+            }
+            else {
+                attributesAvailable -= 100;
+                return 100;
+            }
+        } else {
+            System.out.println("Plus de point d'attribut; " + attributeName + " = " + currentAttributeValue);
+            return currentAttributeValue;
         }
     }
 
@@ -262,12 +336,12 @@ public class Arena {
         System.out.println("-------------------------------------");
         System.out.println("\nJoueur "+ indexPlayer +" - " + getPersonageNameFromList(indexPlayer) +", Veuillez choisir votre attaque :");
         System.out.println("-------------------------------------");
-        System.out.println("1 : "+ getPersonageList(indexPlayer).getAttacksType().basicAttack());
-        System.out.println("2 : "+ getPersonageList(indexPlayer).getAttacksType().specialAttack());
+        System.out.println("1 : "+ getPersonageList(indexPlayer).getAttacksType().basicAttack() + " ("+ getPersonageList(indexPlayer).getBasicDamageToString() + ")");
+        System.out.println("2 : "+ getPersonageList(indexPlayer).getAttacksType().specialAttack() + " (" + getPersonageList(indexPlayer).getSpecialDamageToString() + ")");
     }
 
     /**
-     * @param fightTurn the turn number (One player attack during each turn)
+     * @param fightTurn the turn number (Only one player attack during each turn)
      * @return the index of the Player that will attack.
      */
     private int getAssailantIndex(int fightTurn){

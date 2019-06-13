37154582e0f6e4f085653f03f99453a696d6584c
clorenzato <cyril.lorenzato@gmail.com>
2019-06-13 15:21:13 +0200

    Few new Tests of the Arena Class : IncreaseAttributes, ChooseAttribute, SelectPersonageClass, SelectPersonageLevel

--- tests/com/lorenzato/arena/ArenaTest.java
+++ tests/com/lorenzato/arena/ArenaTest.java
@@ -2,6 +2,7 @@ package com.lorenzato.arena;
 
 import com.lorenzato.exeptions.NoPersonnageException;
 
+import com.lorenzato.personnage.Personage;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -40,7 +41,7 @@ class ArenaTest {
     }
 
     @Test
-    void Given_BadEntries_When_SelectPersonageClass_Then_CatchErrs() {
+    void Given_BadEntries_When_SelectPersonageClass_Then_CatchErrMessages() {
         System.setIn(new ByteArrayInputStream(String.format("a%n25%n2%n100%n20%n20%n20%n").getBytes()));
         Arena arena = new Arena(1,100);
         arena.choosePersonageClass(1);
@@ -50,7 +51,60 @@ class ArenaTest {
     }
 
     @Test
-    void Given_GoodEntries_When_ChooseAttributeNoAttributeAvailable_Then_ProposeClassInGoodOrder() {
+    void Given_ChoseWarriorValueIs1_When_SelectPersonageClass_Then_GetWarriorClassInList() {
+        System.setIn(new ByteArrayInputStream(String.format("1%n100%n20%n20%n30%n30%n").getBytes()));
+        Arena arena = new Arena(1,100);
+        arena.addPlayersInArena();
+        Personage personage = arena.getPersonageList(1);
+        String[] classOfPersonage = personage.getClass().toString().split("\\.");
+        assertEquals("Warrior", classOfPersonage[classOfPersonage.length-1]);
+    }
+
+    @Test
+    void Given_ChoseProwlerValueIs2_When_SelectPersonageClass_Then_GetProwlerClassInList() {
+        System.setIn(new ByteArrayInputStream(String.format("2%n100%n20%n20%n30%n30%n").getBytes()));
+        Arena arena = new Arena(1,100);
+        arena.addPlayersInArena();
+        Personage personage = arena.getPersonageList(1);
+        String[] classOfPersonage = personage.getClass().toString().split("\\.");
+        assertEquals("Prowler", classOfPersonage[classOfPersonage.length-1]);
+    }
+    @Test
+    void Given_ChoseMageValueIs3_When_SelectPersonageClass_Then_GetMageClassInList() {
+        System.setIn(new ByteArrayInputStream(String.format("3%n100%n20%n20%n30%n30%n").getBytes()));
+        Arena arena = new Arena(1,100);
+        arena.addPlayersInArena();
+        Personage personage = arena.getPersonageList(1);
+        String[] classOfPersonage = personage.getClass().toString().split("\\.");
+        assertEquals("Mage", classOfPersonage[classOfPersonage.length-1]);
+    }
+
+    @Test
+    void Given_BadEntries_When_SelectPersonageLevel_Then_CatchErrMessages() {
+        System.setIn(new ByteArrayInputStream(String.format("101%n0%n100%n").getBytes()));
+        Arena arena = new Arena(1,100);
+        int level = arena.choosePersonageLevel();
+        String[] errOutput = errContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals("Veuillez choisir un niveau < 100", errOutput[0]);
+        assertEquals("Veuillez choisir un niveau > 0", errOutput[1]);
+        assertEquals(100, level);
+    }
+
+
+    @Test
+    void Given_GoodEntries202010_When_ChooseAttributeWithAttributeAvailable50_Then_AttributesAre202010() {
+        System.setIn(new ByteArrayInputStream(String.format("1%n50%n20%n20%n10%n").getBytes()));
+        Arena arena = new Arena(1,100);
+        arena.addPlayersInArena();
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals("Brutus je suis le Guerrier du Joueur 1," +
+                " de niveau 50. Je possède 250 de vitalité, 20 de force," +
+                " 20 d'agilité et 10 d'intelligence !",
+                output[18]);
+    }
+
+    @Test
+    void Given_GoodEntries_When_ChooseAttributeWithNoAttributeAvailable_Then_AttributeIs0() {
         System.setIn(new ByteArrayInputStream(String.format("20%n").getBytes()));
         Arena arena = new Arena(1,100);
         int valueAttribute = arena.chooseAttribute("AttributeName");
@@ -60,17 +114,27 @@ class ArenaTest {
     }
 
     @Test
-    void Given_ALetterAndANumber_When_CallingChooseNumber_Then_ReceiveAnErrorMessageReturnTheGoodNumber() {
+    void Given_GoodEntries_When_IncreaseAttributeWithNoAttributeAvailable_Then_AttributeIsCurrentAttribute() {
+        System.setIn(new ByteArrayInputStream(String.format("20%n").getBytes()));
+        Arena arena = new Arena(1,100);
+        int valueAttribute = arena.increaseAttribute("AttributeName", 15);
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals("Plus de point d'attribut; AttributeName = 15", output[8]);
+        assertEquals(15, valueAttribute);
+    }
+
+    @Test
+    void Given_ALetterAndANumber_When_CallingChooseNumber_Then_ReceiveAnErrorMessageAndReturnTheGoodNumber() {
         System.setIn(new ByteArrayInputStream(String.format("a%n25%n").getBytes()));
         Arena arena = new Arena(1,100);
         int numberChosen = arena.chooseNumber();
         String[] errOutput = errContent.toString().replace("\r\n", "\n").split("\n");
-        assertEquals(25,numberChosen);
         assertEquals("Veuillez saisir un nombre, svp",errOutput[0]);
+        assertEquals(25, numberChosen);
     }
 
     @Test
-    void Given_NoPersonageSelected_When_CallingLetsFight_Then_ThrowExceptionNoPersonnageException(){
+    void Given_NoPersonageSelected_When_CallingLetsFight_Then_ThrowNoPersonnageException(){
         Arena arena = new Arena(1,100);
         assertThrows(NoPersonnageException.class, arena::letsFight);
     }
